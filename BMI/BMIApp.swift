decadcf0696c55d582758ae606839b8fad1a1701
AofPatiphan <patiphan.ma@hotmail.com>
2022-10-19 20:36:42 +0700

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Patiphan Manawanich on 19/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
