c950219bcef9843b39c373337a84118e465b766e
AofPatiphan <patiphan.ma@hotmail.com>
2022-10-20 03:41:53 +0700

    first commit

--- BMI/ContentView.swift
+++ BMI/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var result:String = "BMI Calculator"
+    @State var height:String = ""
+    @State var weight:String = ""
+    @State var error:String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Text(result)
+                    .padding(.vertical)
+                HStack {
+                    Text("น้ำหนัก")
+                        .padding()
+                    TextField("กิโลกรัม" ,text:$weight)
+                        .padding()
+                        .keyboardType(.numberPad)
+                }
+                
+                HStack {
+                    Text("ส่วนสูง")
+                        .padding()
+                    TextField("เซนติเมตร" ,text:$height)
+                        .padding()
+                        .keyboardType(.numberPad)
+                }
+                
+                Button("เริ่มคำนวณ") {
+                    if(self.weight == "" || self.height == ""){
+                        return self.error = "Please input Data"
+                    }
+                    self.result = String(Double(self.weight)!/pow(Double(self.height)!/100.0,2))
+                    self.error = ""
+                }
+                .padding()
+                Text(error)
+                    .padding()
+                    .foregroundColor(.red)
+            }.padding(.horizontal, 30.0)
         }
-        .padding()
     }
 }
 
